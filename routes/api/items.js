81fa95de3c325542cdc1090d439d43b0eca504cb
YaroslavW <doctor355@yahoo.com>
2018-12-10 12:22:56 +0300

    GET.POST,DELETE

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,7 +15,17 @@ router.get('/', (req, res) => {
 });
 
 // @route   POST api/items
-// @desc    Create A POST
+// @desc    Create An Item
+// @access  Public
+
+router.delete('/:id', (req, res) => {
+    Item.findById( req.params.id )
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
+});
+
+// @route   DELETE api/items/:id
+// @desc    DELETE A ITEM
 // @access  Public
 
 router.post('/', (req, res) => {
